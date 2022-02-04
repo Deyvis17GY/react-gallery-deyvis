0227db22f192393a41f241d51d9808fb87f6746e
Deyvis17 <deyvisjaime17@gmail.com>
2022-02-04 01:46:22 -0500

    💄 validate if exist image

--- src/pages/ImageDetail.js
+++ src/pages/ImageDetail.js
@@ -1,39 +1,47 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import { useParams, useHistory } from 'react-router';
+import axios from "axios"
+import React, { useEffect, useState } from "react"
+import { useParams, useHistory } from "react-router"
 export const ImageDetail = () => {
+  const params = useParams()
+  const history = useHistory()
+  const [image, setImage] = useState({
+    title: "",
+    url: "",
+    _id: ""
+  })
 
-    const params = useParams();
-    const history = useHistory();
-    const [image, setImage] = useState({
-        title: "",
-        url: "",
-        _id: "",
-    });
+  const [isLoading, setIsLoading] = useState(false)
 
-    const handleDelete = async() => {
-        await axios.delete(`/api/images/${params.id}`);
-        history.push("/");
-    }
+  const handleDelete = async () => {
+    await axios.delete(`/api/images/${params.id}`)
+    history.push("/")
+  }
 
-    useEffect(() => {
-        (async () => {
-           const res = await axios.get(`/api/images/${params.id}`)
-           setImage(res.data)
-        })();
-    }, [params.id]);
+  useEffect(() => {
+    ;(async () => {
+      const res = await axios.get(`/api/images/${params.id}`)
+      setImage(res.data)
+      setIsLoading(true)
+    })()
+  }, [params.id])
 
-    return (
-        <div className="row">
-            <div className="col-md-4 offset-md-4">
-                <div className="card bg-dark">
-                <img src={image.url} alt={image.title} className="img-fluid" />
-                    <div className="card-body">
-                        <h3>{image.title}</h3>
-                        <button className="btn btn-outline-danger" onClick={handleDelete}>Delete</button>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className='row'>
+      <div className='col-md-4 offset-md-4'>
+        <div className='card bg-dark'>
+          {isLoading ? (
+            <img src={image.url} alt={image.title} className='img-fluid' />
+          ) : (
+            <p className='text-center'>Loading...</p>
+          )}
+          <div className='card-body'>
+            <h3>{image.title}</h3>
+            <button className='btn btn-outline-danger' onClick={handleDelete}>
+              Delete
+            </button>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
--- src/pages/ImageGallery.js
+++ src/pages/ImageGallery.js
@@ -1,27 +1,36 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import {useHistory} from 'react-router-dom';
+import axios from "axios"
+import React, { useEffect, useState } from "react"
+import { useHistory } from "react-router-dom"
 export const ImageGallery = () => {
+  const [images, setImages] = useState([])
+  const history = useHistory()
 
-    const [images, setImages] = useState([]);
-    const history = useHistory();
-   
-    useEffect( () => {
-     (async() => {
-        const res = await axios.get('/api/images');
-        setImages(res.data); 
-    })();
-    }, [])
+  useEffect(() => {
+    ;(async () => {
+      const res = await axios.get("/api/images")
+      setImages(res.data)
+    })()
+  }, [])
 
-    return (
-        <div className="row">
-            {images.map((image, index) => (
-                <div className="col-md-3 p-2 card-image" key={index} 
-                onClick={() => history.push(`/images/${image._id}`)} >
-                    <h6 className="card-title">{image.title}</h6>
-                    <img src={image.url} alt={image.title} className="img-fluid" />
-                </div>
-            ))}
+  return images.length > 0 ? (
+    <div className='container-image'>
+      {images.map((image, index) => (
+        <div
+          className='container-image-item'
+          key={index}
+          onClick={() => history.push(`/images/${image._id}`)}
+        >
+          {/* <h6 className='card-title'>{image.title}</h6> */}
+          <img
+            title={image.title}
+            src={image.url}
+            alt={image.title}
+            className='w-100'
+          />
         </div>
-    )
+      ))}
+    </div>
+  ) : (
+    <h2>Loading...</h2>
+  )
 }
