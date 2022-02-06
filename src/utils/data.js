862ee710d31a22509be6a05314d03dbd0d39da92
Deyvis17 <deyvisjaime17@gmail.com>
2022-02-06 16:53:14 -0500

    feat: add baseUrl to axios

---
+++ src/utils/data.js
@@ -0,0 +1,12 @@
+import axios from "axios"
+
+export const baseHttps = axios.create({
+  baseURL: process.env.REACT_APP_ENDPOINT,
+  timeout: 3000,
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers":
+      "Origin, X-Requested-With, Content-Type, Accept"
+  }
+})
